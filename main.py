4f447febfd4ceb26c1c339aefae0047f600d3111
Miguel <mgiljimenez@gmail.com>
2024-05-30 11:34:19 +0200

    bug model

--- main.py
+++ main.py
@@ -440,12 +440,12 @@ elif selected == "Modelo IA":
         submitted = st.form_submit_button("Submit", use_container_width=True)
         if submitted:
             dic_sal_pob={"madrid":[26721,6751251],"barcelona":[24243,5714730],"valencia":[20069,2589312],"sevilla":[17710,1947852],"malaga":[17390,1695651],"alicante":[17139,1881762],"islas baleares":[19834,1173008]}
-            data_to_predict={"Sistema de reserva online": [sistema_reserva_in],"Cantidad métodos de pago":[num_metodo_pago_in],
-            "Reservas última semana":[reservas_semana_in],"Número de fotos online":[num_foto_in],"Afiliado a The Fork":[afiliado_in],
-            "Número de premios":[num_premios_in],"Número de comentarios":[num_comentarios_in],"Número de reseñas":[num_resena_in],
-            "Calidad de comida":[calidad_comida_in],"Calidad del servicio":[calidad_servicio_in],"Calidad del ambiente":[calidad_ambiente_in],
-            "Provincia":[provincia_in],"Población": [dic_sal_pob[provincia_in][1]],"Salario Medio Anual": [dic_sal_pob[provincia_in][0]],
-            "Es Michelin":[michelin_in],"Restaurante con distinción":[distincion_in]}
+            data_to_predict={"Bookable": [sistema_reserva_in],"Cantidad_metodos_pago":[num_metodo_pago_in],
+            "Reservas_last_week":[reservas_semana_in],"Numero_fotos":[num_foto_in],"Is_Affiliated":[afiliado_in],
+            "Numero_awards":[num_premios_in],"Review_count":[num_comentarios_in],"Rating_count":[num_resena_in],
+            "Food_rating":[calidad_comida_in],"Service_rating":[calidad_servicio_in],"Ambience_rating":[calidad_ambiente_in],
+            "Provincia":[provincia_in],"Poblacion": [dic_sal_pob[provincia_in][1]],"Salario Medio Anual": [dic_sal_pob[provincia_in][0]],
+            "Michelin numérico":[michelin_in],"Rate_Distinction_numérico":[distincion_in],"Tipo_comida":[tipo_comida_in]}
             data_to_predict_df = pd.DataFrame(data_to_predict)
             model=joblib.load("models/model_cities/random_forest_model.pkl")
             result=model.predict(data_to_predict_df)
